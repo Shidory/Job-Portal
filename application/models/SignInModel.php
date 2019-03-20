98c28ddb08afdacf1b9da0b41bc464e91bcd90f5
Simon MWEPU <simonmwepu@gmail.com>
2019-03-20 09:39:27 +0200

    set parametters

--- application/models/SignInModel.php
+++ application/models/SignInModel.php
@@ -1,7 +1,8 @@
 <?php
     class SignInModel extends CI_Model{
 
-        public function sign_in(){
+        public function sign_in($name, $surname, $title, $adress, $email,$phone, $genre, $date, $nationality, 
+            $civilstate, $image, $pseudo, $pwd){
             
         }
     }
