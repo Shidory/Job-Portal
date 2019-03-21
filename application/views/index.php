08fb96435db06d887685baf482db330c9eb5e9f8
Simon MWEPU <simonmwepu@gmail.com>
2019-03-21 14:41:30 +0200

    add link for font

--- application/views/index.php
+++ application/views/index.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?= base_url() ?>static/css/materialise.css">
-    <link rel="stylesheet" href="../../static/css/fonts/font.css">
+    <link rel="stylesheet" href="<?= base_url() ?>static/css/fonts/font.css">
     <script src="../../static/js/materialize.js"></script>
     <link rel="stylesheet" href="../../static/css/style.css">
     <title>Job Portal</title>
