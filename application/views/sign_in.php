e233d09837cd8cfc9eb21ac37ea5374677831b65
Simon MWEPU <simonmwepu@gmail.com>
2019-03-21 14:58:42 +0200

    bootstrap min

--- application/views/sign_in.php
+++ application/views/sign_in.php
@@ -671,7 +671,7 @@
 
     <script src="<?= base_url() ?>vendors/jquery/dist/jquery.min.js"></script>
     <script src="<?= base_url() ?>vendors/popper.js/dist/umd/popper.min.js"></script>
-    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?= base_url() ?>vendors/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="assets/js/main.js"></script>
 
 
