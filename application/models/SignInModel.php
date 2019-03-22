49f1ebb185319a962f961f47035a08e73cdd7ff5
Simon MWEPU <simonmwepu@gmail.com>
2019-03-22 04:58:24 +0200

    [UPDATE] update all input name

--- application/models/SignInModel.php
+++ application/models/SignInModel.php
@@ -1,11 +1,11 @@
 <?php
     class SignInModel extends CI_Model{
 
-        public function sign_in($name, $surname, $title, $adress, $email,$phone, $genre, $date, $nationality, 
-            $civilstate, $image, $pseudo, $pwd){
+        public function sign_in($nom, $prenom, $titre, $adresse, $email,$telephone, $genre, $datenaiss, $nationalite, 
+            $etatcivil, $image, $pseudo, $pwd){
 
-            $request = "INSERT INTO demandeur VALUES($name, $surname, $title, $adress, $email,$phone, $genre, $date, $nationality, 
-                $civilstate, $image, $pseudo, $pwd)";
+            $request = "INSERT INTO demandeur VALUES($nom, $prenom, $titre, $adresse, $email,$telephone, $genre, $datenaiss, $nationalite, 
+            $etatcivil, $image, $pseudo, $pwd)";
         }
     }
 
