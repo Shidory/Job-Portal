3203754ce22f262a72cd06e83fdbf64672fbe3a8
Simon MWEPU <simonmwepu@gmail.com>
2019-03-22 16:15:02 +0200

    wanna insert datas

--- application/models/SignInModel.php
+++ application/models/SignInModel.php
@@ -1,12 +1,12 @@
 <?php
     class SignInModel extends CI_Model{
 
-        public function sign_in($nom, $prenom, $titre, $adresse, $email,$telephone, $genre, $datenaiss, $nationalite, 
-            $etatcivil, $image, $pseudo, $pwd){
+        public function sign_in($nom, $prenom, $titre, $adresse, $email, $telephone, $genre, $datenaiss, 
+		$nationalite, $pseudo, $pwd){
 
-            $request = "INSERT INTO demandeur VALUES($nom, $prenom, $titre, $adresse, $email,$telephone, $genre, $datenaiss, $nationalite, 
-            $etatcivil, $image, $pseudo, $pwd)";
-        }
-    }
-
+            $request = "INSERT INTO demandeur VALUES($nom, $prenom, $titre, $adresse, $email, $telephone, $genre, $datenaiss, 
+            $nationalite, $pseudo, $pwd)";
+            $prepare = array(
+                
+            )
 ?>
