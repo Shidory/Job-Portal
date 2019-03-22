8308f24035134ce42c82d304fbb314cc30f73e55
Simon MWEPU <simonmwepu@gmail.com>
2019-03-22 16:37:40 +0200

    [ADD]different important links

--- application/views/login.php
+++ application/views/login.php
@@ -294,17 +294,17 @@
     <!-- Right Panel -->
 
     <script src="<?= base_url(); ?>vendors/jquery/dist/jquery.min.js"></script>
-    <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
-    <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="assets/js/main.js"></script>
+    <script src="<?= base_url(); ?>vendors/popper.js/dist/umd/popper.min.js"></script>
+    <script src="<?= base_url(); ?>vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/main.js"></script>
 
 
-    <script src="vendors/chart.js/dist/Chart.bundle.min.js"></script>
-    <script src="assets/js/dashboard.js"></script>
-    <script src="assets/js/widgets.js"></script>
-    <script src="vendors/jqvmap/dist/jquery.vmap.min.js"></script>
-    <script src="vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
-    <script src="vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script src="<?= base_url(); ?>vendors/chart.js/dist/Chart.bundle.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/dashboard.js"></script>
+    <script src="<?= base_url(); ?>assets/js/widgets.js"></script>
+    <script src="<?= base_url(); ?>vendors/jqvmap/dist/jquery.vmap.min.js"></script>
+    <script src="<?= base_url(); ?>vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <script src="<?= base_url(); ?>vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
     <script>
         (function($) {
             "use strict";
