fbe3be69cc02b515a97823c5d3a49af42a57115c
Simon MWEPU <simonmwepu@gmail.com>
2019-03-22 08:36:19 +0200

    [NEW] create function upload_profile

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -99,8 +99,14 @@ class Welcome extends CI_Controller {
 		}
 	}
 
+	#####################################################################
 	public function login(){
-		
+
+	}
+
+	#####################################################################
+	public function upload_profile(){
+
 	}
 }
 ?>
