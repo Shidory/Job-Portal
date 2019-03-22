46d7e55b90bfdae788e2293ec439e8abf71a5ed9
Simon MWEPU <simonmwepu@gmail.com>
2019-03-22 16:34:55 +0200

    [ADD]link sytle for style

--- application/views/login.php
+++ application/views/login.php
@@ -24,7 +24,7 @@
     <link rel="stylesheet" href="<?= base_url(); ?>vendors/jqvmap/dist/jqvmap.min.css">
 
 
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
