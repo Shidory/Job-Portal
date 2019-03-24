1ceb9c2c01565a99b9cbdb6402ca66b9bd90b6d9
Simon MWEPU <simonmwepu@gmail.com>
2019-03-24 13:53:46 +0200

    [ADD] getter

--- application/models/structure/Competences.php
+++ application/models/structure/Competences.php
@@ -7,7 +7,16 @@
         private $fk_idDemandeur;
 
         public Competences($idCompetences, $nomCompetences, $fk_idDemandeur){
-            
+
+            $this->idCompetences = $idCompetences;
+            $this->nomCompetences = $nomCompetences;
+            $this->fk_idDemandeur = $id_idDemandeur;
         }
+
+        public function getIdCompetences(){
+            return $this->idCompetences;
+        }
+
+        
     }
 ?>
