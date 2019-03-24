54ff5c9df696f6f6253cddbd15d3fbbcc5ae18b4
Simon MWEPU <simonmwepu@gmail.com>
2019-03-24 21:07:47 +0200

    [ADD]initialisation of all variables

--- application/models/structure/Emplois.php
+++ application/models/structure/Emplois.php
@@ -11,6 +11,12 @@
 
         public Emplois($idEmplois,$fk_idDemandeur, $nomEntreprise, $posteEmplois, $dateDebutEmplois, $dateFinEmplois){
 
+            $this->idEmplois = $idEmplois;
+            $this->fk_idDemandeur = $fk_idDemandeur;
+            $this->nomEntreprise = $nomEntreprise;
+            $this->posteEmplois = $posteEmplois;
+            $this->dateDebutEmplois = $dateDebutEmplois;
+            $this->dateFinEmplois = $dateFinEmplois;
         }
     }
 ?>
