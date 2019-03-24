d8d41be052f577386b2a3760b67e461ad768431c
Simon MWEPU <simonmwepu@gmail.com>
2019-03-24 20:39:34 +0200

    [UPDATE]view

--- application/views/home.php
+++ application/views/home.php
@@ -442,24 +442,6 @@
                                 <h4 class="card-title mb-0">Traffic</h4>
                                 <div class="small text-muted">October 2017</div>
                             </div>
-                            <!--/.col-->
-                            <div class="col-sm-8 hidden-sm-down">
-                                <button type="button" class="btn btn-primary float-right bg-flat-color-1"><i class="fa fa-cloud-download"></i></button>
-                                <div class="btn-toolbar float-right" role="toolbar" aria-label="Toolbar with button groups">
-                                    <div class="btn-group mr-3" data-toggle="buttons" aria-label="First group">
-                                        <label class="btn btn-outline-secondary">
-                                            <input type="radio" name="options" id="option1"> Day
-                                        </label>
-                                        <label class="btn btn-outline-secondary active">
-                                            <input type="radio" name="options" id="option2" checked=""> Month
-                                        </label>
-                                        <label class="btn btn-outline-secondary">
-                                            <input type="radio" name="options" id="option3"> Year
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-                            <!--/.col-->
 
 
                         </div>
@@ -603,15 +585,7 @@
                 </div>
             </div>
 
-            <div class="col-xl-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>World</h4>
-                    </div>
-                    <div class="Vector-map-js">
-                        <div id="vmap" class="vmap" style="height: 265px;"></div>
-                    </div>
-                </div>
+    
                 <!-- /# card -->
             </div>
 
