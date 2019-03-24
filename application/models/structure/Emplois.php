04051f6c41364a24bf63be7860cb654b8818b547
Simon MWEPU <simonmwepu@gmail.com>
2019-03-24 20:59:42 +0200

    [ADD] definition of attributes

--- application/models/structure/Emplois.php
+++ application/models/structure/Emplois.php
@@ -1,6 +1,12 @@
 <?php 
 
     class Emplois{
-        
+
+        private idEmplois;
+        private fk_idDemandeur;
+        private nomEntreprise;
+        private posteEmplois;
+        private dateDebutEmplois;
+        private dateFinEmplois;
     }
 ?>
