fa80364fa43b0fdeba61e897c148144c65e2f192
Simon MWEPU <simonmwepu@gmail.com>
2019-03-24 13:43:51 +0200

    [ADD]constructor

--- application/models/structure/Competences.php
+++ application/models/structure/Competences.php
@@ -5,5 +5,9 @@
         private $idCompetences;
         private $nomCompetences;
         private $fk_idDemandeur;
+
+        public Competences($idCompetences, $nomCompetences, $fk_idDemandeur){
+            
+        }
     }
 ?>
