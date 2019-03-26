4c392b8187d481aec4029aeb9bb4be1d1066f076
Simon MWEPU <simonmwepu@gmail.com>
2019-03-26 15:06:48 +0200

    [ADD] request

--- application/models/dao/DemandeurDao.php
+++ application/models/dao/DemandeurDao.php
@@ -4,6 +4,9 @@
 
         public function sign_in($demandeur){
 
+            $request = "INSERT INTO demandeur VALUES(O, :nomDemandeur, :prenomDemandeur, :titre, :adresseDemandeur, :emailDemandeur,
+            telephoneDemandeur, :genre, :dateNaissance, :nationalite, :etatCivil, :imageProfile, :pseudo, :pwd,
+            etat )";
         }
     }
 ?>
