8949720269708ae03f39985a8127d0c5d6aa3654
Simon MWEPU <simonmwepu@gmail.com>
2019-03-26 15:08:55 +0200

    [ADD] prepare request

--- application/models/dao/DemandeurDao.php
+++ application/models/dao/DemandeurDao.php
@@ -7,6 +7,8 @@
             $request = "INSERT INTO demandeur VALUES(O, :nomDemandeur, :prenomDemandeur, :titre, :adresseDemandeur, :emailDemandeur,
             telephoneDemandeur, :genre, :dateNaissance, :nationalite, :etatCivil, :imageProfile, :pseudo, :pwd,
             etat )";
+
+            $prepare = $this->db->prepare($request);
         }
     }
 ?>
