45ca0a01985ef6a0d4d5263bec3c0f842556f0aa
Simon MWEPU <simonmwepu@gmail.com>
2019-03-27 17:57:54 +0200

    [ADD] table ... demandeur

--- application/models/DemandeurDao.php
+++ application/models/DemandeurDao.php
@@ -3,7 +3,7 @@
     class DemandeurDAO extends CI_Model{
 
             public function sign_up($data){
-
+                $table = "demandeur";
                 return $this->db->insert($this->table, $data);
             }
         }
