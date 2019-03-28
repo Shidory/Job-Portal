7d929431e3720cba66a050758ff86b60b54da29b
Simon MWEPU <simonmwepu@gmail.com>
2019-03-28 20:26:46 +0200

    [NEW] function construct

--- application/models/DemandeurDAO.php
+++ application/models/DemandeurDAO.php
@@ -2,13 +2,22 @@
 
     class DemandeurDAO extends CI_Model{
 
+        public function __construct(){
+            
+        }
+        #################################################################
         public function M_sign_up($data){
             $table = "demandeur";
             return $this->db->insert($table, $data);
         }
 
+        #################################################################
         public function M_login($data){
-            
+
+            $table = "demandeur";
+            return $this->db->insert($table, $data);
         }
+
+        #################################################################
     }
 ?>
