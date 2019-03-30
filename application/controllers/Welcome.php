d24d3aa4dbc57a682094f274b0ef278da3048415
Simon MWEPU <simonmwepu@gmail.com>
2019-03-30 16:48:31 +0200

    [DELETE] method do_upload

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -37,32 +37,6 @@ class Welcome extends CI_Controller {
 
 		$this->load->view('home');
 	}
-
-	#####################################################################
-	public function do_upload()
-	{
-			$config['upload_path']          = base_url("upload/profile");
-			$config['allowed_types']        = 'gif|jpg|png';
-			$config['max_size']             = 100;
-			$config['max_width']            = 1024;
-			$config['max_height']           = 768;
-
-			$this->load->library('upload', $config);
-
-			if ( ! $this->upload->do_upload('image'))
-			{
-					$error = array('error' => $this->upload->display_errors());
-
-					$this->load->view('sign_in', $error);
-			}
-			else
-			{
-					$data = array('upload_data' => $this->upload->data());
-					
-					$this->load->view('upload_success', $data);
-			}
-	}
-
 	#####################################################################
 	public function C_sign_up(){
 		
