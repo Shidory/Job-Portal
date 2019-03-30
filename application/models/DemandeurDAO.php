a8dd586f192cf3f34d8775f0072404c81c18159c
Simon MWEPU <simonmwepu@gmail.com>
2019-03-30 16:41:53 +0200

    [ADD] get email

--- application/models/DemandeurDAO.php
+++ application/models/DemandeurDAO.php
@@ -2,20 +2,38 @@
 
     class DemandeurDAO extends CI_Model{
 
+        #################################################################
         public function __construct(){
-            
+
+            parent::__construct();
+
+            $table_dem="demandeur";
+
         }
+
         #################################################################
         public function M_sign_up($data){
-            $table = "demandeur";
-            return $this->db->insert($table, $data);
+
+            return $this->db->insert($table_dem, $data);
         }
 
         #################################################################
         public function M_login($data){
 
-            $table = "demandeur";
-            return $this->db->insert($table, $data);
+            $request = $this->db->select($table_dem, 'email', 'pwd');
+
+            if ('email'==$data[0] AND 'pwd'==$data[1]){
+
+                $this->load->view('home');
+            }
+        }
+
+        #################################################################
+        function get_by_email($email){
+
+            $this->db->order_by($this->id, 'DESC');
+            $this->db->where('emailDemandeur',$email);
+            return $this->db->get($this->table_dem)->row();
         }
 
         #################################################################
