3282d0172386729d67a007eca457a0d8fd5accbf
Simon MWEPU <simonmwepu@gmail.com>
2019-03-30 22:16:08 +0200

    [ADD]link controller

--- application/views/login.php
+++ application/views/login.php
@@ -23,43 +23,53 @@
     </nav>
 </div>
 
-    <div class="row">
-        <div class="col s12 m6 offset-m3">
-            <div class="card">
-                <div class="card-content black-text">
-                    <span class="card-title center">Login</span>
-                    <div class="divider"></div>
-                    <?= form_open('welcome/C_login','class="col s12'); ?>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <i class="material-icons prefix">mail_outline</i>
-                                <input name="email" id="icon_prefix" type="email" class="validate">
-                                <label for="icon_prefix">Email</label>
+<div class="row">
+    <div class="col s12 m6 offset-m3">
+        <div class="card">
+            <div class="card-content black-text">
+                <span class="card-title center blue-grey-text"><!--?php echo ucfirst($title);?--></span>
+                <div class="divider"></div>
+                <?= form_open('welcome/C_login','class =""');?>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">mail_outline</i>
+                            <input id="icon_prefix" type="email" class="validate" name="email" required>
+                            <label for="icon_prefix">Email <span class="red-text">*<span></label>
+                        </div>
+                        <?php echo form_error('email') ?></span>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">lock</i>
+                            <input id="icon_telephone" type="password" class="validate" name="pwd" required>
+                            <label for="icon_telephone">Password <span class="red-text">*<span></label>
+                        </div>
+                        <!-- <span class="input-field col s12"><?php echo form_error('pwd') ?></span> -->
+                        <div class="left">
+                            <!-- Switch -->
+                            <div class="switch">
+                                Type :
+                                <label>
+                                    Personne
+                                    <input type="checkbox" name="typePers" value="on">
+                                    <span class="lever"></span>
+                                </label>
+                            </div>
+                            <div class="switch">
+                                <label>
+                                    Entreprise
+                                    <input type="checkbox" name="typeEnt" value="on">
+                                    <span class="lever"></span>
+                                </label>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <i class="material-icons prefix">lock</i>
-                                <input name="pwd" id="icon_telephone" type="password" class="validate">
-                                <label for="icon_telephone">Password</label>
-                            </div>
-                            <div class="left">
-                                <!-- Switch -->
-                                <div class="switch">
-                                    Type :
-                                    <label>
-                                        Personne
-                                        <input type="checkbox" name="type">
-                                        <span class="lever"></span> Entreprise
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <button type="submit" class="waves-effect waves-light btn center">Login</button>
-                    <?= form_close(); ?>
-                </div>
+                    </div>
+                    <button class="btn waves-effect waves-light center" type="submit" name="login" id="" value="SeConnecter">Se Connecter</button>
+                </form>
+                <span class="red-text">(*) <strong style="font-size :12px;">champs obligatoire</strong><span>
             </div>
         </div>
     </div>
+</div>
 </body>
 </html>
