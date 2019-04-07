483b4b3b3ac1ccfc5d5e7c37abb684aaa142b16d
Simon MWEPU <simonmwepu@gmail.com>
2019-04-07 12:33:45 +0200

    [ADD]fix bug

--- application/views/_inc/footer.php
+++ application/views/_inc/footer.php
@@ -7,28 +7,19 @@
     </div>
 </footer>
 <script>
+
     document.addEventListener('DOMContentLoaded', function() {
         var elems = document.querySelectorAll('.collapsible');
         var instances = M.Collapsible.init(elems);
 
         var elems = document.querySelectorAll('select');
-        var instances = M.FormSelect.init(elems);
-
-        var elems = document.querySelectorAll('.modal');
-        var instances = M.Modal.init(elems);
+        var instances = M.FormSelect.init(elems);;
 
         var elems = document.querySelectorAll('.datepicker');
         var options = {
             format: "yyyy-mm-dd"
         };
         var instances = M.Datepicker.init(elems, options);
-
-        var elems = document.querySelectorAll('.tooltipped');
-        var instances = M.Tooltip.init(elems, options);
-
-        var elems = document.querySelectorAll('.sidenav');
-        var instances = M.Sidenav.init(elems);
-  });
     });
 </script>
 <!-- Pour l'affiche du Toast de message -->
